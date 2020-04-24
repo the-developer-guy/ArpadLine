9fe1821b448aaf2144116e7b82d644fdc1697811
Béla Durucz <mod4sw@gmail.com>
2020-04-24 18:36:48 +0200

    1ms task
    Added 1ms task in control panel.

--- ControlPanel/ControlPanel.ino
+++ ControlPanel/ControlPanel.ino
@@ -1,3 +1,10 @@
+#define TASK_1_MS 1
+
+void task_1_ms(void);
+
+unsigned long systemTime = 0;
+unsigned long counter_task_1_ms = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +12,13 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  systemTime = millis();
+  if(systemTime > counter_task_1_ms){
+    counter_task_1_ms += TASK_1_MS;
+    task_1_ms();
+  }
+}
 
+void task_1_ms(void){
+  
 }
