2c44df23eda594f539bcdf0828aabe90505edf54
current88 <35607753+current88@users.noreply.github.com>
2020-05-08 17:16:44 +0200

    refactor functions
    újra gondoltam a függvények neveit, és funkcióját. eseményeket töröltem, illetve 1-2 új is bejött. kidolgozás folyamatban

--- ControlPanel/SystemState.h
+++ ControlPanel/SystemState.h
@@ -13,65 +13,35 @@ typedef enum SystemState{/*eseményeket felvinni*/
   SYSTEMSTATE_MAINTANANCE
 };
 
-void systemEventHandler_sabotage(int zoneNumber, int userLevel);
+void systemEventHandler_sabotage(int zoneLevel, int userLevel);
 /*
- Zóna szabotázs esetén hívjuk meg. A "zóna szabotázs" függvény hívja meg, átadja a szabotázsban lévő zóna számát.
- Ha az authUser = 0, akkor meghívjuk a "riasztás" függvényt. Különben:
+ Zóna szabotázs esetén hívjuk meg. A zóna kezelő osztály hívja meg, átadja a szabotázsban lévő zóna számát, és ha van a bejelentkezett felhasználó szintjét.
+ Ha a userLevel = 0, akkor meghívjuk a "riasztás" függvényt. Különben:
    - Ha a SystemState:
      - SYSTEMSTATE_INIT: Ha a korábbi SystemState nem SYSTEMSTATE_INIT vagy SYSTEMSTATE_MAINTANANCE. Akkor meghívjuk a "riasztás" függvényt. Különben nem történik semmi.
      - SYSTEMSTATE_DISARMED: Ha az userLevel:
                              - 0 (Nincs belépve senki): Akkor meghívjuk a "riasztás" függvényt.
                              - 1 (Telepítő): Nem történik semmi.
-                             - 2 (Admin): Meghívjuk a "zonePermissionLevel" függvényt a zonaNumber-rel és a userLevel-lel, ez visszaad egy boolean-t. 
-                                         Ha a "zonePermissionLevel" függvény igaz, akkor nem történik semmi. Különben meghívjuk a "riasztás" függvényt.
+                             - 2 (Admin): Ha a zoneLevel az admin szintnek megfelelő, akkor nem történik semmi, ha annál magasabb akkor hívjuk az "alarm" függvényt
                              - 3 (Emelt szintű user): Akkor meghívjuk a "riasztás" függvényt.
                              - 4 (User): Akkor meghívjuk a "riasztás" függvényt.
      - SYSTEMSTATE_ARMED: Akkor meghívjuk a "riasztás" függvényt. 
      - SYSTEMSTATE_SILENT_ALARM: Akkor meghívjuk a "riasztás" függvényt. 
      SystemState = SYSTEMSTATE_TAMPER
  */
-void systemEventHandler_auth(int userLevel);/*Ez minek is kell????*/
+void systemEventHandler_alarm(int zoneLevel, bool delayed);
 /*
     - Ha a SystemState:
-     - SYSTEMSTATE_DISARMED: Ha az userLevel:
-                             - 0 (Nincs belépve senki, élesítő kód): 
-                             - 1 (Telepítő): Nem történik semmi.
-                             - 2 (Admin): Meghívjuk a "zonePermissionLevel" függvényt a zonaNumber-rel és a userLevel-lel, ez visszaad egy boolean-t. 
-                                         Ha a "zonePermissionLevel" függvény igaz, akkor nem történik semmi. Különben meghívjuk a "riasztás" függvényt.
-                             - 3 (Emelt szintű user): Akkor meghívjuk a "riasztás" függvényt.
-                             - 4 (User): Akkor meghívjuk a "riasztás" függvényt.
-     - SYSTEMSTATE_ARMED: Akkor meghívjuk a "riasztás" függvényt. 
-     - SYSTEMSTATE_SILENT_ALARM: Akkor meghívjuk a "riasztás" függvényt. 
-     - SYSTEMSTATE_ALARM: 
-     - SYSTEMSTATE_SILENT_TAMPER:
-     - SYSTEMSTATE_PANIC: 
-     - SYSTEMSTATE_ARMING_DELAYED:
- */
-void systemEventHandler_alarm();
-/*
-    - Ha a SystemState:
-     - SYSTEMSTATE_DISARMED, SYSTEMSTATE_INIT, SYSTEMSTATE_MAINTANANCE, SYSTEMSTATE_SILENT_PANIC, SYSTEMSTATE_ALARM: figyelmen kívül hagyjuk.
-     - SYSTEMSTATE_ARMING_DELAYED: propertyben meghatározott delayTime elteltére vár NTC szerin
-     - Minden más esetben "riasztás"
+     - SYSTEMSTATE_DISARMED, SYSTEMSTATE_INIT, SYSTEMSTATE_DOWNLOAD, SYSTEMSTATE_MAINTANANCE, SYSTEMSTATE_SILENT_PANIC, SYSTEMSTATE_ALARM: figyelmen kívül hagyjuk.
+     - Minden más esetben a delayed idő kivárása után "riasztás"
      SystemState = SYSTEMSTATE_ALARM
-     Amíg az RTC nem éri el a property-ben meghatározott silent időt, addíg riaszt, 
-     utánna meghívni a systemEventHandler_silentAlarm() függvényt.
  */
-void systemEventHandler_silentAlarm(); /*Kell ez nekünk? Vagy saját maga vizsgálja a sileenthez szükséges időt*/
+void systemEventHandler_silentAlarm(bool panic, int delayToAlarm); /*Kell ez nekünk? Vagy saját maga vizsgálja a silenthez szükséges időt*/
 /*
   - Propertyben meghatározott silentAlarmDelay eltelte után a riasztást néma állapotra teszi.
   SystemState = SYSTEMSTATE_SILENT_ALARM
  */
-void systemEventHandler_panicAlarm();/*param: user level*/
-/*  
-  SystemState = SYSTEMSTATE_PANIC
-  meghívni a systemEventHandler_alarm() függvényt
- */
-void systemEventHandler_silentTamper();/*param: user level*/
-/*
-  
- */
-byte systemEventHandler_arm();
+byte systemEventHandler_arm(int userLevel, bool delayed);
 /*
  Ezt az autentikáció után hívjuk meg. 
  Hiba kóddal térünk vissza:
@@ -81,5 +51,10 @@ byte systemEventHandler_arm();
  - 3: Ha a "zóna szabotázs" függvény nem 0-val tér vissza. Ez esetben nem élesítünk.
  SystemState változik SYSTEMSTATE_ARMED.  
  */
+byte systemEventHandler_disarm(int userLevel);
+/*
+ 
+ */
 void systemEventHandler_init();
-void systemEventHandler_disarm();
+void systemEventHandler_downloadeMode();
+void systemEventHandler_maintenanceMode();
